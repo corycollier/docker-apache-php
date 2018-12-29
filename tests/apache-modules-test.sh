ffeb69aa4662abf224bb49c7219c03c39716a9f8
Cory Collier <corycollier@corycollier.com>
2018-12-29 17:36:33 -0500

    adding tests

--- tests/apache-modules-test.sh
+++ tests/apache-modules-test.sh
@@ -2,7 +2,7 @@
 
 set -o errexit
 set -o nounset
-set -o pipefail
+# set -o pipefail
 # set -o xtrace
 
 # First, lets do some sanity checking
@@ -14,54 +14,17 @@ fi
 declare _name="${1}"
 declare _actual_modules=
 declare _check_modules=(
-  "Core"
-  "ctype"
-  "curl"
-  "date"
-  "dom"
-  "fileinfo"
-  "filter"
-  "ftp"
-  "gd"
-  "hash"
-  "iconv"
-  "json"
-  "libxml"
-  "mbstring"
-  "mysqli"
-  "mysqlnd"
-  "openssl"
-  "pcre"
-  "PDO"
-  "pdo_mysql"
-  "pdo_sqlite"
-  "Phar"
-  "posix"
-  "readline"
-  "Reflection"
-  "session"
-  "SimpleXML"
-  "SPL"
-  "sqlite3"
-  "standard"
-  "tokenizer"
-  "xdebug"
-  "xml"
-  "xmlreader"
-  "xmlwriter"
-  "xsl"
-  "zip"
-  "zlib"
-  "Xdebug"
+  "rewrite"
+  "php7_module"
 )
 
 # Store the actual modules in a local variable
-_actual_modules=$(docker exec "${_name}" bash -c "php -m")
+_actual_modules=$(docker exec "${_name}" bash -c "apachectl -M 2>&1")
 
 # Iterate over our check modules to make sure they're in the container
 for _module in ${_check_modules[@]}; do
   if [[ -n $(echo "${_actual_modules}" | grep "${_module}") ]]; then
-    echo "[PASS] - php has module [${_module}]"
+    echo "[PASS] - apache has module [${_module}]"
   else
     echo "[ERROR] - module [${_module}] not found"
   fi
